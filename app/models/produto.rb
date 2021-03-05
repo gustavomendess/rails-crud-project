472cea72ba9c58ab6f4f4d922090a9df3a498fb0
Gustavo Kureck Mendes <gustavo@portabilis.com.br>
2021-03-05 09:36:40 -0300

    Melhora funcionamento e aplica validaçoes para departamento nos produtos

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,6 +2,6 @@ class Produto < ActiveRecord::Base
 
   belongs_to :departamento
 
-  validates :preco, presence: true
+  validates :preco, :departamento, presence: true
   validates :nome, length: { minimum: 4 }
 end
